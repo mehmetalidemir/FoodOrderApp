4c61d5d7d3dd2687004e1ee0c6300bf25faafdc2
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2023-02-08 14:25:20 +0300

    Initial commit

---
+++ FoodOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrderApp
+//
+//  Created by Mehmet Ali Demir on 8.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
