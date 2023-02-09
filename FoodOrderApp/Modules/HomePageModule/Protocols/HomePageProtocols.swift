944c8f5654b54ecc7902d593234a5a6aea7a6920
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2023-02-09 20:20:00 +0300

    homepage module created

--- FoodOrderApp/Modules/HomePageModule/Protocols/HomePageProtocols.swift
+++ FoodOrderApp/Modules/HomePageModule/Protocols/HomePageProtocols.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol ViewToPresenterHomePageProtocol {
+    var homePageInteractor: PresenterToInteractorHomePageProtocol? { get set }
+    var homePageView: PresenterToViewHomePageProtocol? { get set }
+
+}
+
+protocol PresenterToInteractorHomePageProtocol {
+    var homePagePresenter: InteractorToPresenterHomePageProtocol? { get set }
+
+}
+
+protocol InteractorToPresenterHomePageProtocol {
+}
+
+protocol PresenterToViewHomePageProtocol {
+}
+
+
+protocol PresenterToRouterHomePageProtocol {
+    static func createModule(ref: ViewController)
+}
